0c266be48c2771cee97c1f801da4252f249e1fa5
NoName <skarr.age@mail.ru>
2019-08-09 11:38:53 +0300

    spring.5.basic.sope

--- src/main/java/springcourse/TestSpring.java
+++ src/main/java/springcourse/TestSpring.java
@@ -12,7 +12,13 @@ public class TestSpring {
 //
 //        MusicPlayer musicPlayer=new MusicPlayer(music);
         MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer1=context.getBean("musicPlayer",MusicPlayer.class);
+        boolean comparison=musicPlayer==musicPlayer1;
+
+        musicPlayer.setVolume(10);
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer1.getVolume());
+//        musicPlayer.playMusic();
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
         context.close();
